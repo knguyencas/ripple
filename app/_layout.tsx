0213e2e1242820012e6aea4ff73b2cc8d7226155
knguyencas <168413274+knguyencas@users.noreply.github.com>
2026-05-04 10:57:37 +0700

    core initialize crypto polyfill

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
+import { setupCrypto } from '../services/core/crypto-polyfill';
+setupCrypto();
+
 import { Stack } from 'expo-router';
 import { useFonts, Nunito_400Regular, Nunito_600SemiBold, Nunito_700Bold } from '@expo-google-fonts/nunito';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import { startPedometerAccumulator } from '../services/tracker/pedometer.service';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -16,10 +18,6 @@ export default function RootLayout() {
   useEffect(() => {
     if (fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded]);
-
-  useEffect(() => {
-    startPedometerAccumulator().catch(() => {});
-  }, []);
 
   if (!fontsLoaded) return null;
 
